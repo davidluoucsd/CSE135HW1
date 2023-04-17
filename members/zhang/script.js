24f18e8332fcfce7c2cc7e70684f515da46001be
Junzhe Luo <j5luo@ucsd.edu>
2023-04-16 23:05:38 -0700

    hw1

---
+++ members/zhang/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var emailLink = document.getElementById('email');
+    emailLink.addEventListener('click', function() {
+        alert('You clicked the email link!');
+    });
+});
