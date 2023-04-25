5a0192a28de3e3cae9d97fa7298acee2c4880d91
Junzhe Luo <j5luo@ucsd.edu>
2023-04-25 12:07:47 -0700

    HW2 ADD google log1

--- members/luo/ss.js
+++ members/luo/ss.js
@@ -1,4 +1,21 @@
+LogRocket.init('e8zsxt/cse135');
+// This is an example script - don't forget to change it!
+LogRocket.identify('75', {
+    name: 'Junzhe Luo',
+    email: 'j5luo@ucsd.edu',
+  
+    // Add your own custom user variables here, ie:
+    subscriptionType: 'pro'
+  });
 
+  // This is an example script - don't forget to change it!
+LogRocket.identify('77', {
+    name: 'Lejia Zhang',
+    email: 'lez002@ucsd.edu',
+  
+    // Add your own custom user variables here, ie:
+    subscriptionType: 'pro'
+  });
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
--- members/script.js
+++ members/script.js
@@ -4,3 +4,21 @@ document.addEventListener('DOMContentLoaded', function() {
         alert('You clicked the email link!');
     });
 });
+LogRocket.init('e8zsxt/cse135');
+// This is an example script - don't forget to change it!
+LogRocket.identify('75', {
+    name: 'Junzhe Luo',
+    email: 'j5luo@ucsd.edu',
+  
+    // Add your own custom user variables here, ie:
+    subscriptionType: 'pro'
+  });
+
+  // This is an example script - don't forget to change it!
+LogRocket.identify('77', {
+    name: 'Lejia Zhang',
+    email: 'lez002@ucsd.edu',
+  
+    // Add your own custom user variables here, ie:
+    subscriptionType: 'pro'
+  });
