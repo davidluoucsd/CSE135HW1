d0007a0aa841ac0c738a7aad3355679e51cc14f5
Junzhe Luo <j5luo@ucsd.edu>
2023-05-26 13:23:51 -0700

    test 9921

--- cookie-consent.js
+++ cookie-consent.js
@@ -1,27 +1,22 @@
-window.addEventListener("DOMContentLoaded", function() {
-    // Check if the "acceptCookies" item is in local storage
-    var acceptCookies = localStorage.getItem("acceptCookies");
-    var cookieConsentBanner = document.getElementById("cookieConsentBanner");
-
-    if (acceptCookies === null) {
-        // If there is no "acceptCookies" item, show the banner
-        cookieConsentBanner.style.display = "block";
+document.addEventListener("DOMContentLoaded", function() {
+    if (!getCookie('cookieConsent')) {
+      var consentBanner = document.getElementById('cookieConsentBanner');
+      var acceptButton = document.getElementById('cookieConsentAccept');
+      var rejectButton = document.getElementById('cookieConsentReject');
+      
+      if(consentBanner && acceptButton && rejectButton) {
+        consentBanner.style.display = 'block';
+  
+        acceptButton.addEventListener('click', function() {
+          consentBanner.style.display = 'none';
+          setCookie('cookieConsent', 'yes', 30);
+        });
+  
+        rejectButton.addEventListener('click', function() {
+          consentBanner.style.display = 'none';
+          setCookie('cookieConsent', 'no', 30);
+        });
+      }
     }
-
-    var cookieConsentAccept = document.getElementById("cookieConsentAccept");
-    var cookieConsentReject = document.getElementById("cookieConsentReject");
-
-    cookieConsentAccept.addEventListener("click", function() {
-        // When the user clicks "Accept", hide the banner and store their choice in local storage
-        cookieConsentBanner.style.display = "none";
-        localStorage.setItem("acceptCookies", "yes");
-        setCookie('cookieConsent', 'yes', 30); 
-    });
-    
-
-    cookieConsentReject.addEventListener("click", function() {
-        // When the user clicks "Reject", hide the banner and store their choice in local storage
-        cookieConsentBanner.style.display = "none";
-        localStorage.setItem("acceptCookies", "no");
-    });
-});
+  });
+  
